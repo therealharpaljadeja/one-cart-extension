efcce0f746f8f177b5604141e146e4222ae53ed3
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-07-19 13:21:53 +0530

    fix: tab url change bug

--- background/index.ts
+++ background/index.ts
@@ -5,19 +5,42 @@ chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error))
 
+// Sidepanel should be activated if the current tab URL matches to the supported Zora URL.
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (tab.status === "loading") {
+    if (changeInfo.url) {
+      if (changeInfo.url.match(ZORA_ORIGIN)) {
+        await chrome.sidePanel.setOptions({
+          tabId,
+          path: "sidepanel.html",
+          enabled: true
+        })
+      } else {
+        await chrome.sidePanel.setOptions({
+          tabId,
+          enabled: false
+        })
+      }
+    }
+  }
+})
+
+// When switching tabs if the active tab URL matches supported Zora URL then Sidepanel should be activated.
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
   chrome.tabs.get(activeInfo.tabId, async (tab) => {
-    if (tab.url.match(ZORA_ORIGIN)) {
-      await chrome.sidePanel.setOptions({
-        tabId: tab.id,
-        path: "sidepanel.html",
-        enabled: true
-      })
-    } else {
-      await chrome.sidePanel.setOptions({
-        tabId: tab.id,
-        enabled: false
-      })
+    if (tab.url) {
+      if (tab.url.match(ZORA_ORIGIN)) {
+        await chrome.sidePanel.setOptions({
+          tabId: tab.id,
+          path: "sidepanel.html",
+          enabled: true
+        })
+      } else {
+        await chrome.sidePanel.setOptions({
+          tabId: tab.id,
+          enabled: false
+        })
+      }
     }
   })
 })
